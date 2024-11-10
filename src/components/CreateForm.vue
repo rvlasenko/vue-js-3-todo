9d140490d855d20d0e72bae797a533eb9b4297a6
Roman Vlasenko <rvlasenko.1997@gmail.com>
2024-11-10 16:15:18 +0400

    Fix small issues.

--- src/components/CreateForm.vue
+++ src/components/CreateForm.vue
@@ -33,7 +33,7 @@ const addTodo = () => {
   <form @submit.prevent="addTodo" class="space-y-6">
     <div>
       <label
-        for="name"
+        for="content"
         class="block text-sm/6 font-medium text-gray-900 after:content-['*'] after:ml-0.5 after:text-red-500"
       >
         Task name
@@ -41,7 +41,7 @@ const addTodo = () => {
       <div class="mt-2">
         <input
           type="text"
-          id="email"
+          id="content"
           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm/6"
           placeholder="e.g. Make a call"
           v-model="inputContent"
@@ -54,7 +54,7 @@ const addTodo = () => {
         Pick a category
       </legend>
       <p class="mt-1 text-sm/6 text-gray-600">How do you classify this task?</p>
-      <div class="mt-6 flex space-x-6">
+      <div class="mt-6 sm:space-x-6 sm:flex">
         <div v-for="category in categories" :key="category.id" class="flex items-center">
           <input
             :id="category.id"
@@ -73,7 +73,7 @@ const addTodo = () => {
 
     <button
       type="submit"
-      class="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      class="rounded-md bg-indigo-600 px-5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     >
       Create
     </button>
